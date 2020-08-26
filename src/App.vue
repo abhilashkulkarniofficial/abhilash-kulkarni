ed20123c0916b9648b11f6ec118a1a2a0a0afb71
abhilashkulkarni <abhilashkulkarniofficial@gmail.com>
2020-08-26 18:19:34 +0530

    Made changes to Home and App

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,59 @@
 <template>
   <v-app>
-    <v-app-bar
+    <!-- <v-app-bar
       app
-      color="primary"
-      dark
+      flat
+      dense
+      color="white"
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+        <v-btn class="ma-2" tile color="indigo" dark small outlined>Profile
+        <v-icon dark right>mdi-emoticon-cool</v-icon>
+        </v-btn>
+        <v-btn class="ma-2" tile color="indigo" dark small outlined>Projects
+        <v-icon dark right>mdi-laptop-chromebook</v-icon>
+        </v-btn>
+    </v-app-bar> -->
 
     <v-main>
-      <HelloWorld/>
+      <Home/>
     </v-main>
+    <v-footer padless class="indigo white--text lighten-1">
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+      <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 white--text"
+          icon
+        >
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+    </v-col>
+  </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Home from './components/Home';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Home,
   },
 
   data: () => ({
-    //
+    icons: [
+        'mdi-facebook',
+        'mdi-github',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
   }),
 };
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-container fill-height fluid>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-avatar size="100">
+      <img
+        src="@/assets/my_avatar.jpeg"
+        alt="John"
+      >
+    </v-avatar>
+      </v-col>
+
+      <v-col class="mb-4">
+        <h4 class="display-2 font-weight-bold mb-3">
+          Abhilash Kulkarni
+        </h4>
+
+        <p class="subheading font-weight-regular">
+          Data Scientist, Full Stack Developer
+        </p>
+      </v-col>
+
+      <!-- <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          What's next?
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(next, i) in whatsNext"
+            :key="i"
+            :href="next.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ next.text }}
+          </a>
+        </v-row>
+      </v-col> -->
+
+    
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+
+    data: () => ({
+      whatsNext: [
+        {
+          text: 'Explore components',
+          href: 'https://vuetifyjs.com/components/api-explorer',
+        },
+        {
+          text: 'Select a layout',
+          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
+        },
+        {
+          text: 'Frequently Asked Questions',
+          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+        },
+      ],
+    }),
+  }
+</script>
